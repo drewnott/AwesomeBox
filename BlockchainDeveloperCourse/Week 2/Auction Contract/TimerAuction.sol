48076f38aae3674b9aad4e1cc0bf1138032c206d
BahamianWiFi <44509333+BahamianWiFi@users.noreply.github.com>
2018-11-08 21:11:34 -0500

    Update TimerAuction.sol

--- BlockchainDeveloperCourse/Week 2/Auction Contract/TimerAuction.sol
+++ BlockchainDeveloperCourse/Week 2/Auction Contract/TimerAuction.sol
@@ -37,7 +37,7 @@ pragma solidity ^0.4.25;
     function end() external ownerOnly 
     {
     // Check that time has expired and can only end once
-    require(!end require(!end && now >= auctionEnd);
+    require(!end && now >= auctionEnd);
 
     // Update internal states and transfer ether to owner
     end = true;
