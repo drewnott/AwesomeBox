c8f9598c12854ce816e3ad211815579e31de7521
BahamianWiFi <44509333+BahamianWiFi@users.noreply.github.com>
2018-11-08 20:56:31 -0500

    Update BaseAuction.sol

--- BlockchainDeveloperCourse/Week 2/Auction Contract/BaseAuction.sol
+++ BlockchainDeveloperCourse/Week 2/Auction Contract/BaseAuction.sol
@@ -1,1 +1,23 @@
+pragma solidity ^0.4.25;
 
+contract BaseAuction {
+
+ address public owner;
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    event AuctionComplete(address winner, uint bid);
+    event BidAccepted(address bidder, uint bid);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    function bid() external payable;
+    function end() external;
+    
+
+}
