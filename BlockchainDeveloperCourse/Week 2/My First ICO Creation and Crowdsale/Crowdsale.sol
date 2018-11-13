116e747ccdca04f440ebdf8e2fa145211656b72d
BahamianWiFi <44509333+BahamianWiFi@users.noreply.github.com>
2018-11-13 17:21:59 -0500

    Update Crowdsale.sol

--- BlockchainDeveloperCourse/Week 2/My First ICO Creation and Crowdsale/Crowdsale.sol
+++ BlockchainDeveloperCourse/Week 2/My First ICO Creation and Crowdsale/Crowdsale.sol
@@ -14,13 +14,6 @@ contract Crowdsale is ERC20 {
     address private wallet; // wallet to hold the ethers
     IERC20 private token; // address of erc20 tokens
 
-   /**
-    * Event for token purchase logging
-    * @param purchaser who paid for the tokens
-    * @param beneficiary who got the tokens
-    * @param value weis paid for purchase
-    * @param amount amount of tokens purchased
-    */
     event TokensPurchased(
         address indexed purchaser,
         address indexed beneficiary,
@@ -28,25 +21,13 @@ contract Crowdsale is ERC20 {
         uint256 amount
     );
 
-    // -----------------------------------------
-    // Public functions (DO NOT change the interface!)
-    // -----------------------------------------
-   /**
-    * @param _rate Number of token units a buyer gets per wei
-    * @dev The rate is the conversion between wei and the smallest and indivisible token unit.
-    * @param _wallet Address where collected funds will be forwarded to
-    * @param _token Address of the token being sold
-    */
    constructor(uint256 _rate, address _wallet, IERC20 _token) public {
        rate = _rate;
        wallet = _wallet;
        token = _token;
+       //Creates objects for user input
     }
-    //I wasn't really sure what I had to use this constructor.
-
-    /**
-    * @dev Fallback function for users to send ether directly to contract address
-    */
+   
     function() external payable {
        buyTokens(msg.sender);
     }
@@ -58,30 +39,15 @@ contract Crowdsale is ERC20 {
         wallet.transfer(msg.value); //Forwards ether from by msg.sender to the wallet
         token.transfer(beneficiary,msg.value*rate); // Transfer tokens to beneficiary wallet
         weiRaised += msg.value; //Update ICO weiRaised
-        // Below are some general steps that should be done.
-        // You need to decide the right order to do them in.
-        //  - Validate any conditions
-        //  - Calculate number of tokens
-        //  - Update any states
-        //  - Transfer tokens and emit event
-        //  - Forward funds to wallet
-
-        // TODO: Your Code Here
         emit TokensPurchased(msg.sender,beneficiary,msg.value,msg.value*rate);
     }
 
-    /**
-    * @dev Checks whether the cap has been reached.
-    * @return Whether the cap was reached
-    */
     function capReached() public view returns (bool) {
-        require(weiRaised == cap, "Please continue to help us as we endevour to meet our goal");
+        if (weiRaised == cap)
         return true;
+        else
+        return false;
     }
-
-    // -----------------------------------------
-    // Internal functions (you can write any other internal helper functions here)
-    // -----------------------------------------
 
 
 }
