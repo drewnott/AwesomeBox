747e4d3e2ada543f4b3180e5ec89aebbfc30afcb
BahamianWiFi <44509333+BahamianWiFi@users.noreply.github.com>
2018-11-29 23:02:41 -0500

    Update CryptoBaller.sol

--- BlockchainDeveloperCourse/Week 3/CryptoBaller.sol
+++ BlockchainDeveloperCourse/Week 3/CryptoBaller.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.5.0;
 
 import './ERC721.sol';
 
@@ -46,7 +46,7 @@ contract CryptoBallers is ERC721 {
     * @param _ballerId uint ID of the Baller to check
     */
     modifier aboveLevel(uint _level, uint _ballerId) {
-      require(ballers[_ballerId].level > _level," Level" + _level + " and up.");
+      require(ballers[_ballerId].level > _level," Level Too Low.");
     
         _;
     }
@@ -59,14 +59,18 @@ contract CryptoBallers is ERC721 {
     * @dev Allows user to claim first free baller, ensure no address can claim more than one
     */
     function claimFreeBaller() public {
-        // TODO add your code
+        require(!claimedFreeBaller[msg.sender], "Only free baller per person.");
+        _createBaller("Baller", 1, 1, 1); //Allows msg.sender to claim a free baller and select the name
+        claimedFreeBaller[msg.sender] = true;
     }
 
     /**
     * @dev Allows user to buy baller with set attributes
     */
     function buyBaller() public payable {
-        // TODO add your code
+        require (msg.value > ballerFee, "The fee is 0.1 ether."); 
+        owner.transfer(msg.value);//Transfer money to contract address
+        _createBaller("Bought Baller", 1, 1, 1);
     }
 
     /**
@@ -79,6 +83,7 @@ contract CryptoBallers is ERC721 {
     * @param _opponentId uint ID that the baller needs to be above
     */
     function playBall(uint _ballerId, uint _opponentId) onlyOwnerOf(_ballerId) public {
+        require ();
        // TODO add your code
     }
 
@@ -87,8 +92,8 @@ contract CryptoBallers is ERC721 {
     * @param _ballerId uint ID of the Baller who's name you want to change
     * @param _newName string new name you want to give to your Baller
     */
-    function changeName(uint _ballerId, string _newName) external aboveLevel(2, _ballerId) onlyOwnerOf(_ballerId) {
-        // TODO add your code
+    function changeName(uint _ballerId, string calldata _newName) external aboveLevel(2, _ballerId) onlyOwnerOf(_ballerId) {
+        ballers[_ballerId].name = _newName;
     }
 
     /**
@@ -98,8 +103,17 @@ contract CryptoBallers is ERC721 {
    * @param _offenseSkill offensive skill of the Baller
    * @param _defenseSkill defensive skill of the Baller
    */
-    function _createBaller(string _name, uint _level, uint _offenseSkill, uint _defenseSkill) internal {
-        // TODO add your code
+    function _createBaller(string memory _name, uint _level, uint _offenseSkill, uint _defenseSkill) internal {
+       Baller memory baller; //Creates Baller object
+       baller.name = _name; //Sets Baller name
+       baller.level = _level; //Sets Baller level
+       baller.offenseSkill = _offenseSkill; //Sets Baller offenseSkill
+       baller.defenseSkill = _defenseSkill; // Sets Baller defenseSkill
+       baller.winCount = 0; //Sets Baller win count
+       baller.lossCount = 0; //Sets Ball loss count
+       ballers.push(baller); //Adds new Baller to array of ballers
+       
+       _mint(msg.sender, ballers.length); //Creates NFT associated with baller and msg.sender
     }
 
     /**
@@ -108,7 +122,7 @@ contract CryptoBallers is ERC721 {
     * @param _baller2 Baller second baller to average
     * @return tuple of level, attack and defense
     */
-    function _breedBallers(Baller _baller1, Baller _baller2) internal pure returns (uint, uint, uint) {
+    function _breedBallers(Baller memory _baller1, Baller memory _baller2) internal pure returns (uint, uint, uint) {
         uint level = _baller1.level.add(_baller2.level).div(2);
         uint attack = _baller1.offenseSkill.add(_baller2.offenseSkill).div(2);
         uint defense = _baller1.defenseSkill.add(_baller2.defenseSkill).div(2);
